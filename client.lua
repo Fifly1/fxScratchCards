4e298b82f071915cd3a10ddd58dc7cbabbf3d587
fifly <107129715+Fifly1@users.noreply.github.com>
2024-05-23 10:16:27 -0700

    vulnerability issue fixed

--- client.lua
+++ client.lua
@@ -1,19 +1,22 @@
 RegisterNetEvent('fx_scratchcards:client:Open')
-AddEventHandler('fx_scratchcards:client:Open', function()
+AddEventHandler('fx_scratchcards:client:Open', function(token)
     SetNuiFocus(true, true)
     SendNUIMessage({
         action = "openScratchCard",
         minPrice = Config.minPrice,
         maxPrice = Config.maxPrice,
-        tryAgainPercentage = Config.tryAgainPercentage
+        tryAgainPercentage = Config.tryAgainPercentage,
+        token = token
     })
 end)
 
 RegisterNUICallback('exit', function()
     SetNuiFocus(false, false)
+    TriggerServerEvent('fx_scratchcard:server:Closed', source)
 end)
 
 RegisterNUICallback('giveMoney', function(data)
     local price = tonumber(data.price)
-    TriggerServerEvent('fx_scratchcard:server:AddMoney', price)
+    local token = data.token
+    TriggerServerEvent('fx_scratchcard:server:AddMoney', token, price)
 end)
