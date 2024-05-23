c66bb8caf9502048aa850d3435f552635c75055f
fifly <107129715+Fifly1@users.noreply.github.com>
2024-05-23 10:16:51 -0700

    vulnerability issue fixed

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 local QBCore = nil
 local ESX = nil
-local xPlayer = nil
+local activeTokens = {}
 
 if Config.framework == "qbcore" then
     QBCore = exports["qb-core"]:GetCoreObject()
@@ -9,7 +9,9 @@ if Config.framework == "qbcore" then
         local src = source
         local Player = QBCore.Functions.GetPlayer(src)
         Player.Functions.RemoveItem(item.name, 1, item.slot)
-        TriggerClientEvent("fx_scratchcards:client:Open", source)
+        local token = math.random(100000, 999999)
+        activeTokens[src] = token
+        TriggerClientEvent("fx_scratchcards:client:Open", src, token)
     end)
 else
     ESX = exports["es_extended"]:getSharedObject()
@@ -17,18 +19,34 @@ else
     ESX.RegisterUsableItem('fx_scratchcard', function(source)
         local xPlayer = ESX.GetPlayerFromId(source)
         xPlayer.removeInventoryItem('fx_scratchcard', 1)
-        TriggerClientEvent("fx_scratchcards:client:Open", source)
+        local token = math.random(100000, 999999)
+        activeTokens[source] = token
+        TriggerClientEvent("fx_scratchcards:client:Open", source, token)
     end)
 end
 
 RegisterServerEvent('fx_scratchcard:server:AddMoney')
-AddEventHandler('fx_scratchcard:server:AddMoney', function(price)
+AddEventHandler('fx_scratchcard:server:AddMoney', function(token, price)
     local src = source
-    if Config.framework == "qbcore" then
-        local Player = QBCore.Functions.GetPlayer(src)
-        Player.Functions.AddMoney("cash", price)
-    else
-        local xPlayer = ESX.GetPlayerFromId(src)
-        xPlayer.addAccountMoney('money', price)
+    if activeTokens[src] and activeTokens[src] == token then
+        if Config.framework == "qbcore" then
+            local Player = QBCore.Functions.GetPlayer(src)
+            if Player and price >= Config.minPrice and price <= Config.maxPrice then
+                Player.Functions.AddMoney("cash", price)
+            end
+        else
+            local xPlayer = ESX.GetPlayerFromId(src)
+            if xPlayer and price >= Config.minPrice and price <= Config.maxPrice then
+                xPlayer.addAccountMoney('money', price)
+            end
+        end
     end
 end)
+
+RegisterServerEvent('fx_scratchcard:server:Closed')
+AddEventHandler('fx_scratchcard:server:Closed', function()
+    local src = source
+    activeTokens[src] = nil
+end)
+
+
