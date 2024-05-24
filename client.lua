5eaa484f00da3ed98d250fa66c33258b8fab13f5
fifly <107129715+Fifly1@users.noreply.github.com>
2024-05-24 13:35:16 -0700

    fixed bugs / added animations

--- client.lua
+++ client.lua
@@ -1,22 +1,25 @@
 RegisterNetEvent('fx_scratchcards:client:Open')
-AddEventHandler('fx_scratchcards:client:Open', function(token)
+AddEventHandler('fx_scratchcards:client:Open', function(prizes)
     SetNuiFocus(true, true)
     SendNUIMessage({
         action = "openScratchCard",
-        minPrice = Config.minPrice,
-        maxPrice = Config.maxPrice,
-        tryAgainPercentage = Config.tryAgainPercentage,
-        token = token
+        resourceName = GetCurrentResourceName(),
+        prizes = prizes
     })
+    if not IsPedInAnyVehicle(PlayerPedId()) then
+        TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_PARKING_METER", 0, true)
+    end
 end)
+
 
 RegisterNUICallback('exit', function()
     SetNuiFocus(false, false)
-    TriggerServerEvent('fx_scratchcard:server:Closed', source)
+    if not IsPedInAnyVehicle(PlayerPedId()) then
+        ClearPedTasksImmediately(PlayerPedId())
+    end
 end)
 
 RegisterNUICallback('giveMoney', function(data)
-    local price = tonumber(data.price)
-    local token = data.token
-    TriggerServerEvent('fx_scratchcard:server:AddMoney', token, price)
+    local slotIndex = tonumber(data.slotIndex)
+    TriggerServerEvent('fx_scratchcard:server:AddMoney', slotIndex)
 end)
